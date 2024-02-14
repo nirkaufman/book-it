67049b44340428ef41912cafe52a5753e0283a9e
nirkaufman <nirkaufman@gmail.com>
2024-02-14 19:39:00 +0200

    add text

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <div>
         <h1>MyBook it Application!</h1>
         <p>Final project for August bootcamp!</p>
+        <p>CI/CD Is simple in 2024</p>
       </div>
   )
 }
