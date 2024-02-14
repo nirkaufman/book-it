79491108c6289414ac45c5661c443992e56486bc
nirkaufman <nirkaufman@gmail.com>
2024-02-14 20:34:59 +0200

    add child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+function Child() {
+  return (
+      <div>
+        <h1>Child Component</h1>
+        <p>Child component content</p>
+      </div>
+  )
+}
+
+
 function App() {
   return (
       <div>
         <h1>MyBook it Application!</h1>
         <p>Final project for August bootcamp!</p>
         <p>CI/CD Is simple in 2024</p>
+        <Child />
       </div>
   )
 }
