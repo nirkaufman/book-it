c40ff38bd299f295a3e818d6db8aa37633d73ee9
nirkaufman <nirkaufman@gmail.com>
2024-02-21 20:51:34 +0200

    changed the home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ function App() {
   return (
       <div>
         <h1>MyBook it Application!</h1>
-        <p>Final project for August bootcamp!</p>
-        <p>CI/CD Is simple in 2024</p>
+        <h2>Some changes</h2>
       </div>
   )
 }
